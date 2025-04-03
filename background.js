4b201c00bea090132b1bcc3383c5b2f182615a1e
aadityasarda <155258250+aadityasarda@users.noreply.github.com>
2025-04-03 19:46:11 +0530

    background.js

--- background.js
+++ background.js
@@ -3,6 +3,3 @@ console.log("Reader Extension Installed");
 chrome.runtime.onInstalled.addListener(() => {
   console.log("Service worker is active");
 });
-
-// Remove or comment out any code that injects content.js manually.
-// We rely on the manifest’s "content_scripts" for injection.
